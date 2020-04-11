f4314ba7dec4cacbdadc2715d3eb638f268fbc81
xpec-kcasasbuenas <kcasasbuenas@formax.cl>
2020-04-11 15:47:27 -0500

    New modules custom by routes

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { PagesRoutingModule } from './pages.routing.module';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    PagesRoutingModule
+  ],
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component
+  ],
+  providers: []
+})
+export class PagesModule { }
