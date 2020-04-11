7c1ab9601147bd3b7b4d0c3fd1e405aadbda9ad4
xpec-kcasasbuenas <kcasasbuenas@formax.cl>
2020-04-11 15:45:41 -0500

    Ordering routes in app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
-import {ProgressComponent} from './pages/progress/progress.component';
+
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './login/register.component';
-import {Graficas1Component} from './pages/graficas1/graficas1.component';
-import {NopagefoundComponent} from './shared/nopagefound/nopagefound.component';
-import {PagesComponent} from './pages/pages.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 const routes: Routes = [
-  {
-    path:'',
-    component:PagesComponent,
-    children:[
-      {path:'dashboard',component:DashboardComponent},
-      {path:'progress',component:ProgressComponent},
-      {path:'graficas1',component:Graficas1Component},
-      {path:'',redirectTo:'/dashboard', pathMatch:'full'}
-    ]
-  },
-
   {path:'register',component:RegisterComponent},
   {path:'login',component:LoginComponent},
   {path:'**',component:NopagefoundComponent}
